6b84612dac0f5abcbdea1a535b86dbe9988c258a
Angel Ozuna-Galvan <60494278+AjBoogie@users.noreply.github.com>
2020-05-16 04:39:33 -0500

    Add files via upload

---
+++ minimum.js
@@ -0,0 +1,17 @@
+/*
+
+Write a function
+min that takes two arguments and returns their minimum.
+
+*/
+
+
+const min = function(a, b) {
+    
+    if(a<b){
+        return a;
+    }
+    else{
+        return b;
+    }
+}
